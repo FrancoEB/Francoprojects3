d59283b02bfdd0f6a03f7fcb63c4735282bceffc
Marco Antonio <Marco Antonio@MarcoAntonio-HP>
2019-05-26 04:55:13 -0500

    Pregunta7

---
+++ Pregunta7_parcial/src/Pregunta_7.java
@@ -0,0 +1,33 @@
+
+public class Pregunta_7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i, x=4,w=9,q;
+		for(i=-1;i<20;i+=3) {
+			x++;
+			for(q=4;q<11;q++) {
+				do {
+					i=+3;
+					w=sizeof(i);
+					i=x+w;
+					x=w+i;
+						
+				} while (x<15);
+			}
+		}
+		System.out.println("x:"+x+"i:"+i);
+
+	}
+
+	private static int sizeof(int i) {
+		// TODO Auto-generated method stub
+		return i;
+	}
+
+	
+	
+	}
+
+
